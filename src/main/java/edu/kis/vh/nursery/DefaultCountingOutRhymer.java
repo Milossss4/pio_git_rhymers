1116f0076ee699f8664765cdcb5b19c00bc1bc14
jaguel <m.jagiello03@gmail.com>
2024-05-13 15:06:00 +0200

    7. Dodano final przy numbers

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,7 +5,7 @@ public class DefaultCountingOutRhymer {
     public static final int CAPACITY = 12;
     public static final int EMPTY = -1;
     public static final int DEFAULT_VALUE = -1;
-    private int[] numbers = new int[CAPACITY];
+    final private int[] numbers = new int[CAPACITY];
 
     public int getTotal() {
         return total;
